bde641047beeba826920706945f54d80bc764157
Keiko Wada <Keiko@example.com>
2017-12-19 18:54:18 -0500

    created game logic

---
+++ Memory_Game/js/main.js
@@ -0,0 +1,22 @@
+/*console.log("User flipped " + cardOne);*/
+/*console.log("user flipped " + cardThree); */
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push("cardOne");
+cardsInPlay.push("cardTwo");
+
+console.log("user flipped queen");
+console.log("User flipped king");
+
+
+if (cardsInPlay.length === 2)
+if (cardsInPlay[0] === cardsInPlay[1]){
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+

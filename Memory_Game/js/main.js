3c5583bfb63726bdd927526c776d9338180e6faa
Keiko Wada <Keiko@example.com>
2018-01-10 18:10:52 -0500

    created game logic

--- Memory_Game/js/main.js
+++ Memory_Game/js/main.js
@@ -19,7 +19,8 @@ var cards = [
 	rank: "king",
 	suit: "diamonds",
 	cardImage: "images/king-of-diamonds.png"
-	},
+	}
+
 	];
 
 
@@ -33,20 +34,53 @@ var checkForMatch = function() {
   }
 };
 
-var flipCard = function(cardId) {
+var flipCard = function (){
+	cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
-   
+
 cardsInPlay.push(cards[cardId].rank);
 
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
+
+this.setAttribute('src', cards[cardId].cardImage);
 
 if (cardsInPlay.length === 2) {
 checkForMatch();
   }
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function (){
+	for (var i = 0; i < cards.length; i++) {
+	
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.setAttribute('id', i);
+	cardElement.addEventListener('click', flipCard); 
+	document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+ 
+createBoard();
+
+var i =0;
+
+function getScore() {
+	 if (cardsInPlay[0] === cardsInPlay[1]) {
+	 	i=i+1;
+	  alert(i);
+	} else{
+		alert(i);
+	}
+
+};
 
 
+function myReset() {
+	for (var j = 0; j < cards.length; j++) {
+		var cardElement = document.getElementById([j]);
+		cardElement.setAttribute('src', 'images/back.png');
+	}
+	cardsInPlay = [];
+}; 
